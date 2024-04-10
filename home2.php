bb38170ceab4568d33ca2368214e54fef1b39ab1
Dominik Ilas <ilasdominik@gmail.com>
2024-04-10 13:54:23 +0200

    VypisProduktov

--- home2.php
+++ home2.php
@@ -1,3 +1,5 @@
+
+
 <?php
 $servername = "localhost";
 $dbname = "dominko_sk";
@@ -8,27 +10,37 @@ echo "pripaja sa ...";
 
 $connection = new mysqli($servername, $username, $password, $dbname);
 
-if ($connection- >connect_error) {
-die("Chyba pripojenie k db: " . $connection- >connect_error);
+if ($connection->connect_error) {
+die("Chyba pripojenie k db: " . $connection->connect_error);
 }
 echo " |Connected|" . "< br / >";
 echo "< br / >";
 
+
+$query = mysqli_query($connection, "SELECT * FROM t_shop");
+while($fetch = mysqli_fetch_array($query)){
+?>
+	<ul>
+		<?php  echo '<img src="data:image/jpeg;base64,'.base64_encode( $fetch['img'] ).'" >';?>
+		<a><?php echo $fetch['name']?></a>
+	</ul>
+<?php    
+}
+
 $sql = "SELECT * FROM t_shop";
 
-$vysledok = $connection- >query($sql);
+$vysledok = $connection->query($sql);
 
-$pocet = $vysledok- >num_rows;
-echo "Pocet riadkov == " . $pocet . "< br / >";
-echo "< br / >";
+$pocet = $vysledok->num_rows;
 
 if ($pocet >0){
-while($riadok = $vysledok- >fetch_assoc() ){
-echo $riadok['id'] . "\t";
+while($riadok = $vysledok->fetch_assoc() ){	
+echo $riadok['ID'] . "\t";
 echo $riadok['name'] . "\t";
 echo $riadok['description'] . "\t";
 echo $riadok['price'] . "\t";
-echo $riadok['img'] . "< br / >";
+
+
 }
 }
 ?>
